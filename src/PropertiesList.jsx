52dc51d73f190f5830302ba21f39a9d3fd6e921a
OlegMoshkovich <oleg.mosh@gmail.com>
2024-03-07 15:07:27 +0100

    add project description

--- src/PropertiesList.jsx
+++ src/PropertiesList.jsx
@@ -4,15 +4,25 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import Accordion from './Accordian';
 
+
+const projects = [
+  {'title' : "TurmfalkeSuites, Andermatt"
+, 'content':"Modern, elegant and sustainable: the TurmfalkeSuites in Andermatt. The wooden construction combines the latest technology, unique comfort with timeless style. Developed to a new standard: equipped with an indoor swimming pool, its own SPA, integrated fitness studio and an indoor golf simulator, the Kestrel Suites offer living and living on an excellent level. The town of Andermatt lies at 1,444 meters in the middle of the Swiss Alps and offers its residents a unique backdrop and countless leisure opportunities - in summer and winter. A place and a project to fall in love with."},
+{'title' : "TurmfalkeSuites, Andermatt"
+, 'content':"Modern, elegant and sustainable: the TurmfalkeSuites in Andermatt. The wooden construction combines the latest technology, unique comfort with timeless style. Developed to a new standard: equipped with an indoor swimming pool, its own SPA, integrated fitness studio and an indoor golf simulator, the Kestrel Suites offer living and living on an excellent level. The town of Andermatt lies at 1,444 meters in the middle of the Swiss Alps and offers its residents a unique backdrop and countless leisure opportunities - in summer and winter. A place and a project to fall in love with."},
+{'title' : "TurmfalkeSuites, Andermatt"
+, 'content':"Modern, elegant and sustainable: the TurmfalkeSuites in Andermatt. The wooden construction combines the latest technology, unique comfort with timeless style. Developed to a new standard: equipped with an indoor swimming pool, its own SPA, integrated fitness studio and an indoor golf simulator, the Kestrel Suites offer living and living on an excellent level. The town of Andermatt lies at 1,444 meters in the middle of the Swiss Alps and offers its residents a unique backdrop and countless leisure opportunities - in summer and winter. A place and a project to fall in love with."},
+
+]
 export default function PropertiesList() {
   return (
     <Stack>
       <List
       spacing={1}
       >
-        {['Info 1','Info 2','Info 3'].map((text, index) => (
+        {projects.map((project, index) => (
             <ListItem  key={`accordian ${index}`}>
-              <Accordion key={text} title={text} content={'Properties table'}/>
+              <Accordion key={index} title={project.title} content={project.content}/>
             </ListItem>
         ))}
       </List>

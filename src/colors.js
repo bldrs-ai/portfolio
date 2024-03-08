01e254361b8e74311cebbf458f4de97b1d2c7152
OlegMoshkovich <oleg.mosh@gmail.com>
2024-03-08 13:36:07 +0100

    Updates

--- src/colors.js
+++
@@ -1,4 +0,0 @@
- export const colors = [
-  {primary: '#F2B138', secondary:'#1436E6', paper: '#434D58', default:'#0D0D0D' },
-  {primary: '#204F2B', secondary:'#FFFE55', paper: '#DBDBDC', default:'#F7F7F7'},
-]

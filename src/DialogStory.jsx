b3653b6b1c0ffb0f39c8fd054f06fd6408339ede
OlegMoshkovich <oleg.mosh@gmail.com>
2024-03-18 09:12:09 +0000

    add expand state to the cards

--- src/DialogStory.jsx
+++ src/DialogStory.jsx
@@ -13,24 +13,29 @@ import Engine from './Logos/Logos_cards/Engine';
 
 const Transition = React.forwardRef((props, ref) => <Slide direction="up" ref={ref} {...props} />);
 
-const InfoCard = ({ title, logo: LogoComponent, description, readMoreLink }) => (
-  <Card sx={{ width: 300, height: 320, borderRadius: 1, backgroundColor: 'white', display: 'flex', flexDirection: 'column' }}>
-    <CardMedia sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '140px' }}>
-      <LogoComponent scale={3.5}/>
-    </CardMedia>
-    <CardContent sx={{ flexGrow: 1 }}>
-      <Typography gutterBottom variant="body1" component="div" textAlign="center">
-        {title}
-      </Typography>
-      <Typography variant="body2" color="text.secondary" textAlign="center">
-        {description}
-      </Typography>
-    </CardContent>
-    <CardActions sx={{ justifyContent: 'flex-end', p: 1 }}>
-      <Button size="small" href={readMoreLink}> Read More  </Button>
-    </CardActions>
-  </Card>
-);
+const InfoCard = ({ title, logo: LogoComponent, description, readMoreLink, expandedDescription }) => {
+  const [expanded, setExpanded] = React.useState(false);
+  return(
+    <Card sx={{ width: 300, height: expanded ? 500: 360, borderRadius: 1, backgroundColor: 'white', display: 'flex', flexDirection: 'column' }}>
+      { !expanded &&
+        <CardMedia sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100px', marginTop:4 }}>
+        <LogoComponent scale={3.5}/>
+      </CardMedia>
+      }
+        <CardContent sx={{ flexGrow: 1 }}>
+          <Typography gutterBottom variant="overline" component="div" textAlign="center">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary" textAlign="center">
+            {expanded? expandedDescription : description}
+          </Typography>
+        </CardContent>
+        <CardActions sx={{ justifyContent: 'flex-end', p: 1 }}>
+          <Button size="small" href={readMoreLink} sx={{fontSize:12}} onClick={()=>setExpanded(!expanded)}> Read More </Button>
+        </CardActions>
+      </Card>
+  )
+};
 
 const DialogFullPage = () => {
   const [open, setOpen] = React.useState(false);
@@ -38,10 +43,46 @@ const DialogFullPage = () => {
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
   const sections = [
-    { title: 'Build for builders', logo: Builder, description: 'Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.' },
-    { title: 'Git enabled Versions', logo: Versions, description: 'We use github for versioning of the project because it is the best in class versioning system enabling the open source evolution of IT' },
-    { title: 'Issue tracking', logo: Notes, description: 'We leverage GitHub\'s issue system for note-taking and connecting conversations directly to specific elements of the CAD models to enable model based collaboration.' },
-    { title: 'Engine', logo: Engine, description: 'Our engine is designed from group up, offering quick geometry processing, and detailed access to complex IFC/STEP standards all in the browser.' },
+    { title: 'Build for builders',
+      logo: Builder,
+      description: 'Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.',
+      expandedDescription:
+      `Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
+      Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
+      Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
+      Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
+      `
+    },
+    { title: 'Git enabled Versions',
+      logo: Versions,
+      description: 'We use github for versioning of the project because it is the best in class versioning system enabling the open source evolution of IT',
+      expandedDescription:
+      `Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
+      Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
+      Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
+      Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
+      `
+    },
+    { title: 'Issue tracking',
+      logo: Notes,
+      description: 'We leverage GitHub\'s issue system for note-taking and connecting conversations directly to specific elements of the CAD models to enable model based collaboration.',
+      expandedDescription:
+      `Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
+       Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
+       Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
+       Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
+      `
+    },
+    { title: 'Engine',
+      logo: Engine,
+      description: 'Our engine is designed from group up, offering quick geometry processing, and detailed access to complex IFC/STEP standards all in the browser.',
+      expandedDescription:
+      `Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
+      Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
+      Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
+      Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
+      `
+    },
   ];
 
   return (
@@ -80,7 +121,7 @@ const DialogFullPage = () => {
           }}
         >
           {sections.map((section, index) => (
-            <InfoCard key={index} title={section.title} logo={section.logo} description={section.description} />
+            <InfoCard key={index} title={section.title} logo={section.logo} description={section.description} expandedDescription={section.expandedDescription} />
           ))}
         </Box>
       </Dialog>

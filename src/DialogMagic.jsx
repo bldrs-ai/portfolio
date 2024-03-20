ed4f6c1c16a11c28d3c758c982f5e1bb57d71388
OlegMoshkovich <oleg.mosh@gmail.com>
2024-03-20 23:05:05 +0100

    add instructions

--- src/DialogMagic.jsx
+++ src/DialogMagic.jsx
@@ -6,11 +6,13 @@ import {
 import CloseIcon from '@mui/icons-material/Close';
 import LogoB_top from './Logos/LogoB';
 import SmartToyOutlinedIcon from '@mui/icons-material/SmartToyOutlined';
+import Button from '@mui/material/Button';
 
 const Transition = React.forwardRef((props, ref) => <Slide direction="up" ref={ref} {...props} />);
 
 const DialogMagic = () => {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false)
+  const [instruction, setInstruction] = React.useState(false)
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
@@ -39,10 +41,37 @@ const DialogMagic = () => {
         </AppBar>
           {/* Inserting the image */}
           <Stack
-          direction="column" justifyContent="center" alignItems="center" sx={{ width:'100%', height:'100%'}}>
-            <Box sx={{width:'300px', height:'300px'}}>
-              <img src={`${process.env.PUBLIC_URL}/Simpsons.gif`} alt="Example" style={{width:'300px', height:'300px', borderRadius:'20px'}} />
+          direction="column" justifyContent="center" alignItems="center" sx={{ width:'100%', height:'100%'}}
+          spacing={4}
+          >
+            <Box sx={{width:'400px', height:'400px'}}>
+              {instruction ?
+                <Stack
+                direction="column"
+                alignItems='center'
+                justifyContent='center'
+                sx={{width:'400px', height:'400px', borderRadius:'20px'}}
+                >
+                  <Typography variant="overline" sx={{ fontWeight:'bold'}}>
+                    1.Go to Bldrs.ai
+                  </Typography>
+                  <Typography variant="overline" sx={{ fontWeight:'bold'}}>
+                    2. Click on the magic wand
+                  </Typography>
+                  <Typography variant="overline" sx={{ fontWeight:'bold'}}>
+                    3. Input the prompt
+                  </Typography>
+                  <Typography variant="overline" sx={{ fontWeight:'bold'}}>
+                    4. Click create
+                  </Typography>
+              </Stack>:
+              <img src={`${process.env.PUBLIC_URL}/Simpsons.gif`} alt="Example" style={{width:'400px', height:'400px', borderRadius:'20px'}} />
+
+            }
             </Box>
+            <Button variant={instruction?'contained':'outlined'} size='small' sx={{borderRadius:'30px', fontWeight:'bold'}}  color='primary'  onClick={()=>setInstruction(!instruction)}>
+              Instructions
+            </Button>
           </Stack>
       </Dialog>
     </>

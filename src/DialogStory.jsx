1b38272a57aa992e587ac2d0173bd0a4e089c273
OlegMoshkovich <oleg.mosh@gmail.com>
2024-03-20 15:45:18 +0100

    add the description

--- src/DialogStory.jsx
+++ src/DialogStory.jsx
@@ -45,42 +45,41 @@ const DialogFullPage = () => {
   const sections = [
     { title: 'Built for builders',
       logo: Builder,
-      description: 'Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.',
+      description: 'Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant BIM data.',
       expandedDescription:
-      `Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
-      Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
-      Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
-      Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
+      `Our goal is to transform how teams manage and access CAD data in 3D environments,
+      making project versions more accessible and simplifying the sharing and searching of 3D data.
+      This initiative aims to eliminate common obstacles in team collaboration, ensuring that members can effortlessly find and utilize project data contained in digital twins.
+      By enhancing these processes, we aim to enable a more cohesive and efficient workflows.
       `
     },
     { title: 'Git enabled Versions',
       logo: Versions,
-      description: 'We use github for versioning of the project because it is the best in class versioning system enabling the open source evolution of IT',
+      description: 'Versioning is tough, but Git makes it simpler, and GitHub is the top choice with the best tools for Git; that is why our versioning workflows are based on GitHub toolchain.',
       expandedDescription:
-      `Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
-      Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
-      Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
-      Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
+      `We have chosen GitHub for our project versioning due to its unparalleled standing in the versioning system domain, particularly for its pivotal role in the open-source movement within IT.
+      GitHub's platform offers a robust set of tools that facilitate not just the management and tracking of changes across project versions, but also foster a collaborative environment where distributed project teams can contribute and workcollectively.
+      This adoption aligns with our commitment to leveraging mature and avaialble best-in-class technology tools.
       `
     },
     { title: 'Issue tracking',
       logo: Notes,
       description: 'We leverage GitHub\'s issue system for note-taking and connecting conversations directly to specific elements of the CAD models to enable model based collaboration.',
       expandedDescription:
-      `Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
-       Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
-       Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
-       Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
+      `Our approach capitalizes on GitHub's issue tracking system, transforming it into a dynamic tool for note-taking and discussion directly linked to distinct facets of the digital twin models.
+      This strategy fosters a model-based collaboration environment where team members can annotate, raise queries, and provide insights right at the source of the subject matter.
+      By integrating conversation threads with specific model elements, we ensure clarity and context in our communications, significantly enhancing the efficiency of our collaborative efforts.
+      This method not only streamlines problem-solving but also enriches digital twin models with a layer of interactive, collective intelligence, driving more informed decision-making on a project.
       `
     },
     { title: 'Engine',
       logo: Engine,
       description: 'Our engine is designed from group up, offering quick geometry processing, and detailed access to complex IFC/STEP standards all in the browser.',
       expandedDescription:
-      `Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
-      Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
-      Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
-      Our goal is to simplify the access to CAD data and project versions, as well as the sharing and searching of relevant 3D data, to enhance team collaboration in a 3D environment.
+      `Our engine is built from scratch to make working with digtial twins fast and easy, all from the web browser.
+      It's really good at understanding complex rules and formats like IFC and STEP, which are important for building and product designs.
+      This means you can get into deep workflows involving BIM models without needing any proprietory desktop based software.
+      It's all about making things simpler and more accessible for everyone on the team, whether you're working on a big project or just need to look at a detailed design.
       `
     },
   ];
